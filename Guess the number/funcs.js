663d36eb7368af68dbf709c84f54176d225cd4d2
mtnleo <mtnleonardi@gmail.com>
2023-09-01 11:42:22 -0300

    Now it saves the current user, and its score. I still gotta save the whole array, but working on it

--- Guess the number/funcs.js
+++ Guess the number/funcs.js
@@ -1,76 +1,18 @@
+// pregunta a Nahuel. Para storear el usuario que ahora mismo está en la pag
+// debería agarrar y crearle una cookie donde almaceno los datos del objeto
+// usuario como JSON (string), para luego leerlo de la cookie y decodificar
+// el JSON para poder usarlo?
+
 var global_score = 0 // it changes if the player logs in
 var users = new Array()
 var current_user = null
-
-function Overwrite_score() {
-    document.getElementById("score").innerText = "Score: " + global_score
-}
-
-if (window.location.pathname.includes("index.html")) {
-    const formul = document.getElementById("form")
-    var secret_number = Math.floor(Math.random() * 200)
-
-    //check if user is logged
-    if (current_user != null) {
-        global_score = current_user.score
-        Overwrite_score()
-    }
-
-
-    function calculate_separation(mystery, guess) { //this function calculates the % of separation between the guess and the mystery number
-        let percentage
-
-        if (mystery > guess ) {
-            percentage = (guess * 100) / mystery
-        }
-        else if (mystery <= guess) {
-            percentage = (mystery * 100) / guess
-        }
-
-        return percentage
-    }
-
-    formul.addEventListener("submit", (event) => {
-        event.preventDefault()
-
-        let num_guess = document.getElementById("guess").value ?? -1
-        let separation = parseInt(calculate_separation(secret_number, num_guess))
-        
-        document.getElementById("number").innerText = num_guess
-        //provisional --------------------------------------
-        if (num_guess < secret_number) {
-            document.getElementById("info").innerText = "The number is bigger"
-        }
-        else if (num_guess > secret_number) {
-            document.getElementById("info").innerText = "The number is smaller"
-        }
-        // provisional --------------------------------------
-        
-
-        if (num_guess < 0 || num_guess > 200) {
-            document.getElementById("info").innerText = "Guess a number between 0 and 200"
-        }
-        else if (num_guess == secret_number) {
-            document.getElementById("info").innerText = "You won! Guess again to start a new game"
-            global_score++
-            Overwrite_score()
-            secret_number = Math.floor(Math.random() * 200)
-            console.log(global_score)
-        }
-
-        
-        //document.getElementById("info").innerText = ""
-        
-    })
-}
-////////////////////////////////
 
 class User {
     constructor(username, pass, email) {
         this.username = username;
         this.pass = pass;
         this.email = email;
-        this.score = 10;
+        this.score = 0;
     }
 
     getUsername() {
@@ -96,6 +38,19 @@ class User {
     }
 }
 
+function set_local_user(logged_user) {
+    localStorage.setItem("logged_user", JSON.stringify(logged_user))
+
+}
+
+function set_user_array() {
+    //
+}
+
+function get_local_user() {
+    return JSON.parse(localStorage.getItem("logged_user"))
+}
+
 
 if (window.location.pathname.includes("signup.html")) {
     // SIGN UP -------------------------------
@@ -112,8 +67,22 @@ if (window.location.pathname.includes("signup.html")) {
         
         users.push(current_user)
 
+        set_local_user(current_user)
+
         console.log("Registrado")
     })
+
+    ///hardcode to login
+    var user1 = new User("tin", "asdasd", "tin@mail.com")
+    var user2 = new User("herni", "asdasd", "herni@mail.com")
+    users.push(user1)
+    users.push(user2)
+
+    user1.increaseScore()
+    user1.increaseScore()
+    user1.increaseScore()
+
+    ////////////////////////
 
     // LOG IN -------------------------------
     const login = document.getElementById("form_login")
@@ -138,17 +107,84 @@ if (window.location.pathname.includes("signup.html")) {
         const login_pass = document.getElementById("pass_login").value
 
         current_user = searchLogin(login_username, login_pass)
+        set_local_user(current_user)
+
 
         if (current_user === null) {
             alert("Couldn't log in. Try again.")
-            console.log("No se pudo loguear")
+            console.log("Couldn't log in")
         }
         else {
+            console.log("Logged. Score -> ", current_user.score)
             alert("Welcome! " + current_user.username)
-            console.log("Logueado")
             global_score = current_user.score
             window.location.href = "index.html";
         }
 
     })
+
+    
+    
 }
+
+function Overwrite_score() {
+    document.getElementById("score").innerText = "Score: " + global_score
+}
+
+if (window.location.pathname.includes("index.html")) {
+    const formul = document.getElementById("form")
+    var secret_number = Math.floor(Math.random() * 200)
+
+    try {
+        current_user = get_local_user()
+    } catch(error) {
+        console.log(error)
+        current_user = null
+    }
+    //check if user is logged
+    if (current_user != null) {
+        console.log("User score -> ", current_user.score)
+        global_score = current_user.score
+        Overwrite_score()
+    }
+    else {
+        console.log("No user here ")
+    }
+
+
+    formul.addEventListener("submit", (event) => {
+        event.preventDefault()
+
+        let num_guess = document.getElementById("guess").value ?? -1
+      
+        document.getElementById("number").innerText = num_guess
+        //provisional --------------------------------------
+        if (num_guess < secret_number) {
+            document.getElementById("info").innerText = "The number is bigger"
+        }
+        else if (num_guess > secret_number) {
+            document.getElementById("info").innerText = "The number is smaller"
+        }
+        // provisional --------------------------------------
+        
+
+        if (num_guess < 0 || num_guess > 200) {
+            document.getElementById("info").innerText = "Guess a number between 0 and 200"
+        }
+        else if (num_guess == secret_number) {
+            document.getElementById("info").innerText = "You won! Guess again to start a new game"
+            global_score++
+            Overwrite_score()
+            secret_number = Math.floor(Math.random() * 200)
+            console.log(global_score)
+            set_local_user(current_user)
+        }
+
+        
+        //document.getElementById("info").innerText = ""
+        
+    })
+}
+////////////////////////////////
+
+
